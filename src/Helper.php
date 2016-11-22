d4d5265a0e2a32daff46c78c3653400ed826d1d6
Maximiliano Jimenez <jimenez@maximiliano.com.ar>
2016-11-22 10:54:00 -0300

    Added documentation comments

--- src/Helper.php
+++ src/Helper.php
@@ -5,21 +5,43 @@ namespace JimenezMaximiliano\LaravelPostman;
 class Helper
 {
     const CONTROLLER_STRING_INDEX = 0;
+    const POSTMAN_SCHEMA = 'https://schema.getpostman.com/json/collection/'
+            . 'v2.0.0/collection.json';
     
+    /**
+     * Class constructor
+     */
     public function __construct()
     {
     }
     
-    public function addTrailingSlash($url)
+    /**
+     * Adds a trailing slash to the given path if it isn't already there
+     * 
+     * @param string $path
+     * @return string
+     */
+    public function addTrailingSlash($path)
     {
-        return $url  . (substr($url, -1) === '/' ?: '/');
+        return $path  . (substr($path, -1) === '/' ?: '/');
     }
     
+    /**
+     * Replaces laravel route parameters format with Postman parameters format
+     * 
+     * @param string $path
+     * @return string
+     */
     public function replaceGetParameters($path)
     {
         return str_replace(['{', '}'], [':', ''], $path);
     }
     
+    /**
+     * Returns the API base URL
+     * 
+     * @return string
+     */
     public function getBaseURL()
     {
         $configURL = config('laravelPostman.apiURL');
@@ -32,6 +54,11 @@ class Helper
         return $this->addTrailingSlash(config('app.url'));
     }
     
+    /**
+     * Returns the API prefix string
+     * 
+     * @return string
+     */
     public function getApiPrefix()
     {
         $apiPrefix = config('laravelPostman.apiPrefix');
@@ -39,7 +66,16 @@ class Helper
         return !empty($apiPrefix) ? $apiPrefix : 'api';
     }
     
-    public function getCollectionStructure($collectionName, $collectionDescription)
+    /**
+     * Returns a postman collection structure array
+     * 
+     * @param string $collectionName
+     * @param string $collectionDescription
+     * @return array
+     */
+    public function getCollectionStructure(
+            $collectionName, 
+            $collectionDescription)
     {
         return [
             'variables' => [],
@@ -47,29 +83,44 @@ class Helper
                 'name' => $collectionName,
                 '_postman_id' => uniqid(),
                 'description' => $collectionDescription,
-                'schema' => 'https://schema.getpostman.com/json/collection/v2.0.0/collection.json',
+                'schema' => self::POSTMAN_SCHEMA,
             ],
             'item' => [],
         ];
     }
     
+    /**
+     * Obtains a postman folder name from the given laravel route
+     * 
+     * @param Illuminate\Routing\Route $route
+     * @return string
+     */
     public function getRouteFolder($route)
     {
         $actionStringParts = explode('@', $route->getActionName());
+        
         if (count($actionStringParts) === 1) {
+            
            return 'Others'; 
         }
+        
         $fullController = $actionStringParts[self::CONTROLLER_STRING_INDEX];
         $controllerClass = explode('\\', $fullController);
         
         return  str_replace('Controller', '', last($controllerClass));
     }
     
+    /**
+     * Returns the path where the exported file will be located
+     * 
+     * @return string
+     */
     public function getExportDirectory()
     {
         $exportDirectory = config('laravelPostman.exportDirectory');
         
         if (empty($exportDirectory)) {
+            
             return $exportDirectory;
         }
         
